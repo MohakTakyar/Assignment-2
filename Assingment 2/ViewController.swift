e85ae5188520bc8cec72040d260f5a66bbb5b4ee
MohakTakyar <156115409+MohakTakyar@users.noreply.github.com>
2024-01-23 13:52:36 -0800

    Initial Commit

---
+++ Assingment 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assingment 2
+//
+//  Created by user238229 on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
